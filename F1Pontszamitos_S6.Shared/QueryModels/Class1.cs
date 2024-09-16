1e3cea15e00bec132243e1f39db4b9fe18b5083e
Tamás Márk Bagosi <bagosimark2002@gmail.com>
2024-09-16 20:39:12 +0200

    class1

---
+++ F1Pontszamitos_S6.Shared/QueryModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Pontszamitos_S6.Shared.QueryModels
+{
+    internal class Class1
+    {
+    }
+}
